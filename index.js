bf2c12ee539683fcaa891f37f9f52b96c7c4e34e
H_A _Opu <wwwhasib4@gmail.com>
2022-04-23 11:08:24 +0600

    setup post and filter by query

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Vallo from smartly-node");
@@ -43,6 +44,26 @@ app.get("/users", (req, res) => {
 app.get("/user/:id", (req, res) => {
   const id = req.params.id;
   const user = users.find((u) => u.id == id);
+  res.send(user);
+});
+// filter by query
+app.get("/users", (req, res) => {
+  if (req.query.name) {
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
+});
+// POST
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
   res.send(user);
 });
 
